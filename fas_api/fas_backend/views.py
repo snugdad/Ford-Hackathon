0a1abf116d7cfd4b306781523c6303fe2604bb1e
scollet <samuelpcollet@gmail.com>
2018-08-25 21:32:47 -0400

    working

--- fas_api/fas_backend/views.py
+++ fas_api/fas_backend/views.py
@@ -12,18 +12,18 @@ from django.core import serializers
 from django.shortcuts import render
 from django.core.cache import cache
 #from django.views.generic.base import View
-from django.contrib.auth import authenticate, login
 #from django.contrib.auth.models import User
 from django.http import HttpResponse, Http404
 from django.views.decorators.cache import cache_page
 from django.contrib.auth.decorators import login_required
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth import authenticate, login, logout
 from django.core.cache.backends.base import DEFAULT_TIMEOUT
 
 from rest_framework.views import APIView
 from rest_framework import authentication
 from rest_framework.response import Response
-from rest_framework.schemas import ManualSchema
+from rest_framework.schemas import ManualSchema, SchemaGenerator
 from rest_framework.authtoken.models import Token
 from rest_framework.compat import coreapi, coreschema
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
@@ -50,30 +50,17 @@ class Download(APIView):
 	download option from request
 	or return 404
 	'''
-#	permission_classes = (IsAuthenticated,)
-#	@login_required
 	def get(self, request, path, format=None):
-		print('error @ fasapi_v.2/fas/fas_backend/views.py line 76')
+#		print('error @ fasapi_v.2/fas/fas_backend/views.py line 76')
 		file_path = os.path.join(settings.APP_ROOT, path)
-		print(file_path)
-#		return None
-#		serializer = FasUserSerializer(data=request.data)
-#		if serializer.is_valid():
-		if request.user.is_authenticated:
-			print('FUGGIN WORKS')
+		if request.user.is_authenticated: print('FUGGIN WORKS')
 		if os.path.exists(file_path):
-    # ziph is zipfile handle
 			ha = hash_dir(file_path)
 			dirPath = file_path.split('/')
 
 			ha_file = file_path + '/' + str(ha) + '.zip'
 
-			print('HA FILE == ', ha_file)
 			shutil.make_archive('app_upload/apps/' + path  + '/' + ha, 'zip', file_path)
-#			zipf = zipfile.ZipFile('app_upload/apps/' + path + '/' + ha + '.zip', 'w', zipfile.ZIP_DEFLATED)
-#			zipdir('app_upload/apps/' + path + '/', zipf)
-#			zipf.close()
-
 
 			with open(ha_file, 'rb') as fh:
 				response = HttpResponse(fh.read(), content_type="application/zip")
@@ -81,7 +68,17 @@ class Download(APIView):
 			os.remove('app_upload/apps/' + path + '/' + ha + '.zip')
 			return response
 		raise Http404
-		#return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class Logout(APIView):
+
+#        if coreapi is not None and coreschema is not None:
+#                schema = get_man_schema('login')
+
+        def post(self, request, format='application/json'):
+                user = logout(request)
+                return Response({
+                                'resp':'user logged out',
+                        })
 
 class Login(APIView):
 
@@ -90,18 +87,13 @@ class Login(APIView):
 
 	def post(self, request, format='application/json'):
 		user = authenticate(username=request.data['username'], password=request.data['password'])
-#		print('USER IS AUTHENTICATED LOGIN == ', user.is_authenticated)
-		serializer = FasUserSerializer(data=request.data)#, context={'request': request})
+		serializer = FasUserSerializer(data=request.data)
 		if serializer.is_valid():
 			user = serializer.save()	
 		if user is not None:
-#			print(request, user)
 			login(request, user)
-			print(user.is_authenticated)
-#			return Response(serializer.data, status=status.HTTP_201_CREATED)
 			return Response({
 				'resp':'user logged in',
-#				'user':serializer.data,
 			})
 		else:
 			return Response({'status':'user invalid'})
@@ -114,20 +106,17 @@ class UserCreate(APIView):
 	if coreapi is not None and coreschema is not None:
 		schema = get_man_schema()
 
-
 	def post(self, request, format='application/json'):
 		serializer = FasUserSerializer(data=request.data, context={'request': request})
 		user = None
-#		permission = getPerm()
 		if serializer.is_valid():
 			user = serializer.save()
-			permission = Permission.objects.get(
-					codename='can_download',
-					name='Can download apps',
-					content_type=ContentType.objects.get_for_app(FasApp),
-				)
-			user.user_permissions.add(permission)
-
+#			permission = Permission.objects.get(
+#					codename='can_download',
+#					name='Can download apps',
+#					content_type=ContentType.objects.get_for_app(FasApp),
+#				)
+#			user.user_permissions.add(permission)
 		if user:
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -141,15 +130,11 @@ def index(request):
 @api_view
 @renderer_classes([renderers.CoreJSONRenderer])
 def schema_view(request):
-    '''
-    generates a scheman of the API
-    endpoints
-    '''
-    generator = schemas.SchemaGenerator(
-					title='Fas API',
-					url='http://fas.42king.com:8197/api/schema/',
-			)
-    return Response(generator.get_schema())
+	generator = SchemaGenerator(
+			title='Fas API',
+			url='https://fas.42king.com/api/schema',
+		)
+	return Response(generator.get_schema())
 
 #@cache_page(CACHE_TTL)
 class FasAppList(generics.ListCreateAPIView):
@@ -162,28 +147,9 @@ class FasAppList(generics.ListCreateAPIView):
 			authentication.TokenAuthentication,
 			authentication.SessionAuthentication
 		)
-#	permission_classes = (IsAuthenticated,)
 
 	def get(self, request, *args, **kwargs):
-#		user = None
-#		request = self.context.get("request")
-#		if request and hasattr(request, "user"):
-#			user = request.user
-#		print(user)
-#		print(args)
-#		print(kwargs)
-#		print('OK FUCKO')
-#		print(request)
-		print(request.auth)
-#		print(request.scheme)
-#		print(request.body)
-#		print(request.META['HTTP_AUTHORIZATION'])
-		
-#		print(request.session)
-#		print(Token.objects.filter(token=request.data['token']).exists)
-#		print(request.data)
-#		print(request.user)
-		print('BIG IF TRU == ', request.user.is_authenticated)
+		print('USER IS AUTHENTICATED ==', request.user.is_authenticated)
 		if request.user.is_authenticated:
 			content = {
 				'status': 'request was permitted',
@@ -208,6 +174,7 @@ class CreateFasAuthToken(ObtainAuthToken):
 			'user_id': user.pk,
 			'email': user.email
 		})
+
 '''
 # TODO : do something with this
 #@cache_page(CACHE_TTL)
@@ -218,7 +185,6 @@ class FasAppDetail(generics.RetrieveUpdateDestroyAPIView):
                       		IsOwnerOrReadOnly,)
 '''
 
-#@cache_page(CACHE_TTL)
 class FasUserList(generics.ListAPIView):
 	'''
 	admin list of all users
